2c6afbefcf2098c50e20c5cb44e5c8ae660facb5
Thomas Luxhøj <tlux-8@hotmail.com>
2022-06-07 14:09:50 +0200

    Implemented Montgomery multiplication in FqBig also and added speed test. Seems like there is room for improvement in Fq.

--- benchmark/FqBenchmark.cpp
+++ benchmark/FqBenchmark.cpp
@@ -62,9 +62,41 @@ static void FqSubBig(benchmark::State &state) {
 		aa - bb;
 	}
 }
+
+static void FqMul(benchmark::State &state) {
+	mpz_t a, b;
+	mpz_inits(a, b, nullptr);
+	gmp_randstate_t rstate;
+	gmp_randinit_default(rstate);
+	mpz_rrandomb(a, rstate, 380);
+	mpz_rrandomb(b, rstate, 380);
+	Fq aa = Fq(a);
+	Fq bb = Fq(b);
+	for (auto _: state) {
+		aa * bb;
+	}
+}
+
+static void FqMulBig(benchmark::State &state) {
+	mpz_t a, b;
+	mpz_inits(a, b, nullptr);
+	gmp_randstate_t rstate;
+	gmp_randinit_default(rstate);
+	mpz_rrandomb(a, rstate, 380);
+	mpz_rrandomb(b, rstate, 380);
+	auto aa = FqBig(a);
+	auto bb = FqBig(b);
+	for (auto _: state) {
+		aa * bb;
+	}
+}
+
+
 // Register the function as a benchmark
 BENCHMARK(FqAdd);
 BENCHMARK(FqSub);
+BENCHMARK(FqMul);
 BENCHMARK(FqAddBig);
 BENCHMARK(FqSubBig);
+BENCHMARK(FqMulBig);
 BENCHMARK_MAIN();
--- src/FqBig.cpp
+++ src/FqBig.cpp
@@ -5,6 +5,11 @@ mpz_class PRIME("1A0111EA397FE69A4B1BA7B6434BACD764774B84F38512BF6730D2A0F6B0F62
 
 mpz_class REDUCER_RECIPROCAL_BIG("14fec701e8fb0ce9ed5e64273c4f538b1797ab1458a88de9343ea97914956dc87fe11274d898fafbf4d38259380b4820", 16);
 
+mpz_class MASK("39402006196394479212279040100143613805079739270465446667948293404245721771497210611414266254884915640806627990306815", 10);
+
+mpz_class FACTOR_BIG("31812345668571394200522959936182890292651532161468729907319195075155855129916747767697203739041175453056836858347517", 10);
+
+
 const int REDUCER_BITS = 384;
 
 void convertIntoMontgomery(mpz_t& value) {
@@ -48,8 +53,22 @@ FqBig FqBig::operator-(const FqBig &rhs) {
 }
 
 FqBig FqBig::operator*(const FqBig &rhs) {
-    mpz_mul(this->value, this->value, rhs.value);
-    return FqBig{this->value};// % PRIME};
+	mpz_t c, temp;
+	mpz_inits(c, temp, nullptr);
+    mpz_mul(c, this->value, rhs.value);
+	mpz_and(temp, c, MASK.get_mpz_t());
+	mpz_mul(temp, temp, FACTOR_BIG.get_mpz_t());
+	mpz_and(temp, temp, MASK.get_mpz_t());
+	mpz_mul(temp, temp, PRIME.get_mpz_t());
+	mpz_add(c, c, temp);
+	mpz_div_2exp(c, c, REDUCER_BITS);
+	int cmp = mpz_cmp(c, PRIME.get_mpz_t());
+	if (cmp < 0) {
+		return FqBig{c};
+	} else {
+		mpz_sub(c, c, PRIME.get_mpz_t());
+		return FqBig{c};
+	}
 }
 
 bool FqBig::operator==(const FqBig &rhs) const {
